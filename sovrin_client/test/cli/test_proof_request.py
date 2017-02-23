b8cbcdf9082e6239a8638421e112da9fd97f4865
Devin Fisher <devin.fisher@evernym.com>
2017-02-23 16:47:40 -0700

    fixed issue with test anoncreds and added cli command show proof request

---
+++ sovrin_client/test/cli/test_proof_request.py
@@ -0,0 +1,6 @@
+from .conftest import aliceCLI
+
+
+def test_show_nonexistant_proof_request(be, do, aliceCLI):
+    be(aliceCLI)
+    do("show proof request Transcript", expect=["No matching proof request(s) found in current keyring"])

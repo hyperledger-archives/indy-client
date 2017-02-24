e7a35ee2530034231f3896da0c51813ede299a21
Jason Law <jason@evernym.us>
2017-02-23 17:12:33 -0700

    skipped unimplemented test

--- sovrin_client/test/cli/test_proof_request.py
+++ sovrin_client/test/cli/test_proof_request.py
@@ -1,6 +1,7 @@
-from .conftest import aliceCLI
+import pytest
 
 
+@pytest.mark.skip('not implemented yet')
 def test_show_nonexistant_proof_request(be, do, aliceCLI):
     be(aliceCLI)
     do("show proof request Transcript", expect=["No matching proof request(s) found in current keyring"])

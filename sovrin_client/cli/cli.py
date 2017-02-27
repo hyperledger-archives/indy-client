e85cb0674ac6227358b2835921408e264f60dc90
rajeshkalaria80 <rajesh.kalaria@gmail.com>
2017-02-27 14:37:44 +0530

    fixed some timing related issues in few test cases, also made script working to run GST against specific environment, though commented now, until it is formally tested under proper story (#57)
    * [SOV-501] added more asserts in poolNodesStarted fixture to make sure dependent test steps only happens once nodes are started, connected and primary is elected
    
    * changed the expected msg on node cli to make sure nodes have selected primary before any dependent tests starts its execution
    
    * [SOV-574] done some refactoring and added few missing test cases observered during working on this bug
    
    * [SOV-574] modified send NODE command example data, added code to show error if any happens when sending send NODE command
    
    * improved the help message for send NODE command
    
    * update 'load invitation' output in GST (#51)
    
    * show 'missing genesis txn' help text only when 'connect test' is trigger
    
    * help text checked moved inside _connetTo method based on code review feedback
    
    * moved node registery condition after environment check
    
    * moved command parsing logic outside of env check as requested in code review
    
    * update 'load invitation' output in GST
    
    * [SOV-589] added test case for testing send NODE command, some other minor refactoring
    
    * refactored help commands
    
    * [SOV-603] increased timeeout for send NODE command to check if that is the reason why it was failing on windows
    
    * [SOV-603] disabling test which is causing some permission issue on windows agent
    
    * [SOV-603] disabling test which is causing some permission issue on windows agent
    
    * removed some commented code, increased timeout for ATTRIB command
    
    * increased timeout for ATTRIB command
    
    * refactoring in help command, some minor usage changes for connect command
    
    * created script which can run getting started tutorial against given environment, this is initial changes, still it needs to be improved
    
    * commented out code until it is completely tested in different scenarios

--- sovrin_client/cli/cli.py
+++ sovrin_client/cli/cli.py
@@ -1499,16 +1499,13 @@ class SovrinCli(PlenumCli):
 
         return defaultdict(lambda: defaultHelper, **mappings)
 
-    def getBasicHelpCmdKeys(self):
-        return ["helpAction", "listAction", "connectTo", "statusAction",
-                "disconnect", "licenseAction", "exitAction"]
+    def getTopComdMappingKeysForHelp(self):
+        return ['helpAction', 'connectTo', 'disconnect', 'statusAction']
 
     def getHelpCmdIdsToShowUsage(self):
-        return ["help", "connect", "list"]
-
+        return ["help", "connect"]
 
     def cmdHandlerToCmdMappings(self):
-
         # The 'key' of 'mappings' dictionary is action handler function name
         # without leading underscore sign. Each such funcation name should be
         # mapped here, its other thing that if you don't want to display it
@@ -1518,6 +1515,10 @@ class SovrinCli(PlenumCli):
 
         mappings = OrderedDict()
         mappings.update(super().cmdHandlerToCmdMappings())
+        mappings['connectTo'] = connectToCmd
+        mappings['disconnect'] = disconnectCmd
+        mappings['addGenTxnAction'] = addGenesisTxnCmd
+        mappings['newIdentifier'] = newIdentifierCmd
         mappings['sendNymAction'] = sendNymCmd
         mappings['sendGetNymAction'] = sendGetNymCmd
         mappings['sendAttribAction'] = sendAttribCmd
@@ -1528,20 +1529,18 @@ class SovrinCli(PlenumCli):
         mappings['showFile'] = showFileCmd
         mappings['loadFile'] = loadFileCmd
         mappings['showLink'] = showLinkCmd
-        mappings['connectTo'] = connectToCmd
-        mappings['disconnect'] = disconnectCmd
         mappings['syncLink'] = syncLinkCmd
         mappings['pingTarget'] = pingTargetCmd
+        mappings['acceptInvitationLink'] = acceptLinkCmd
         mappings['showClaim'] = showClaimCmd
         mappings['reqClaim'] = reqClaimCmd
         mappings['showClaimReq'] = showClaimReqCmd
-        mappings['acceptInvitationLink'] = acceptLinkCmd
-        mappings['addGenTxnAction'] = addGenesisTxnCmd
         mappings['setAttr'] = setAttrCmd
         mappings['sendClaim'] = sendClaimCmd
-        mappings['newIdentifier'] = newIdentifierCmd
 
-        mappings['addGenesisAction'] = None
+        # TODO: These seems to be obsolete, so either we need to remove these
+        # command handlers or let it point to None
+        mappings['addGenesisAction'] = None # overriden by addGenTxnAction
 
         return mappings
 

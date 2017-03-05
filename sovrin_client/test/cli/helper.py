78d30a88b3be32d601e410623c633b5afe8b4d6c
Jason Law <jason@evernym.us>
2017-03-04 21:10:01 -0700

    fix bug with wallet state checker

--- sovrin_client/test/cli/helper.py
+++ sovrin_client/test/cli/helper.py
@@ -219,30 +219,40 @@ def check_wallet(cli,
                  totalClaimsRcvd=None,
                  within=None):
     async def check():
-        assert (totalLinks is None or
-                totalLinks == len(cli.activeWallet._links))
+        actualLinks = len(cli.activeWallet._links)
+        assert (totalLinks is None or (totalLinks == actualLinks)),\
+            'links expected to be {} but is {}'.format(totalLinks, actualLinks)
 
         tac = 0
         for li in cli.activeWallet._links.values():
             tac += len(li.availableClaims)
 
         assert (totalAvailableClaims is None or
-                totalAvailableClaims == tac)
+                totalAvailableClaims == tac), \
+            'available claims {} must be equal to {}'.\
+                format(tac, totalAvailableClaims)
 
-        assert (totalSchemas is None or
-                totalSchemas == len(await cli.agent.prover.wallet.getAllSchemas()))
+        if cli.agent.prover is None:
+            assert (totalSchemas + totalClaimsRcvd) == 0
+        else:
+            w = cli.agent.prover.wallet
+            actualSchemas = len(await w.getAllSchemas())
+            assert (totalSchemas is None or
+                    totalSchemas == actualSchemas),\
+                'schemas expected to be {} but is {}'.\
+                    format(totalSchemas, actualSchemas)
 
-        assert (totalClaimsRcvd is None or
-                totalClaimsRcvd == len((await cli.agent.prover.wallet.getAllClaims()).keys()))
+            assert (totalClaimsRcvd is None or
+                    totalClaimsRcvd == len((await w.getAllClaims()).keys()))
 
     if within:
         cli.looper.run(eventually(check, timeout=within))
     else:
-        check()
+        cli.looper.run(check)
 
 
-def wallet_starting_state(totalLinks=0,
-                          totalAvailableClaims=0,
-                          totalSchemas=0,
-                          totalClaimsRcvd=0):
+def wallet_state(totalLinks=0,
+                 totalAvailableClaims=0,
+                 totalSchemas=0,
+                 totalClaimsRcvd=0):
     return locals()

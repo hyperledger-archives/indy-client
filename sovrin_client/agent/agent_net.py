7b9bed0f7088a1c1945b69b0305f94f3a0af4020
Jason Law <jason@evernym.us>
2017-03-09 23:53:52 -0700

    reorganized parameters

--- sovrin_client/agent/agent_net.py
+++ sovrin_client/agent/agent_net.py
@@ -6,7 +6,7 @@ class AgentNet:
     Mixin for Agents to encapsulate the network interface to communicate with
     other agents.
     """
-    def __init__(self, name, port, basedirpath, msgHandler):
+    def __init__(self, name, port, msgHandler, basedirpath=None):
         if port:
             self.endpoint = Endpoint(port=port,
                                      msgHandler=msgHandler,

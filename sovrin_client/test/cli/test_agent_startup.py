b8fc79f9ed04277b89b7dee5d64f3ca06c6bb6a3
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2017-03-14 13:33:09 +0530

    refactored to use generic port instead of any existing agent port

--- sovrin_client/test/cli/test_agent_startup.py
+++ sovrin_client/test/cli/test_agent_startup.py
@@ -1,23 +1,28 @@
 import pytest
 
-from plenum.common.exceptions import ProdableAlreadyAdded, \
-    PortNotAvailable, OperationError, NoConsensusYet
+from plenum.common.exceptions import PortNotAvailable, OperationError, \
+    NoConsensusYet
+from plenum.common.port_dispenser import genHa
 from plenum.common.types import HA
 from plenum.common.util import randomString, checkPortAvailable
 from plenum.test.conftest import tdirWithPoolTxns
 from sovrin_client.test.agent.conftest import emptyLooper, startAgent
-from sovrin_client.test.agent.faber import createFaber
-from sovrin_client.test.agent.helper import buildFaberWallet
+
+from sovrin_client.test.agent.acme import createAcme as createAgent
+from sovrin_client.test.agent.helper import buildAcmeWallet as agentWallet
+from sovrin_client.test.cli.conftest \
+    import acmeAddedByPhil as agentAddedBySponsor
+
+
+agentPort = genHa()[1]
 
 
 def getNewAgent(name, basedir, port, wallet):
-    return createFaber(name, wallet,
-                basedirpath=basedir,
-                port=port)
+    return createAgent(name, wallet, basedirpath=basedir, port=port)
 
 
 def runAgent(looper, basedir, port, name=None, agent=None):
-    wallet = buildFaberWallet()
+    wallet = agentWallet()
     name = name or "Agent"+ randomString(5)
     agent = agent or getNewAgent(name, basedir, port, wallet)
     agent._name = name
@@ -25,57 +30,54 @@ def runAgent(looper, basedir, port, name=None, agent=None):
 
 
 @pytest.fixture(scope="module")
-def agentStarted(emptyLooper, tdirWithPoolTxns, faberAgentPort):
-    runAgent(emptyLooper, tdirWithPoolTxns, faberAgentPort, "Agent0")
+def agentStarted(emptyLooper, tdirWithPoolTxns):
+    runAgent(emptyLooper, tdirWithPoolTxns, agentPort, "Agent0")
 
 
-def testCreateAgentDoesNotAllocatePort(tdirWithPoolTxns, faberAgentPort):
+def testCreateAgentDoesNotAllocatePort(tdirWithPoolTxns):
     for i in range(2):
-        checkPortAvailable(HA("0.0.0.0", faberAgentPort))
-        getNewAgent("Agent0", tdirWithPoolTxns, faberAgentPort, buildFaberWallet())
-        checkPortAvailable(HA("0.0.0.0", faberAgentPort))
+        checkPortAvailable(HA("0.0.0.0", agentPort))
+        getNewAgent("Agent0", tdirWithPoolTxns, agentPort, agentWallet())
+        checkPortAvailable(HA("0.0.0.0", agentPort))
 
 
-def testStartAgentChecksForPortAvailability(poolNodesStarted, tdirWithPoolTxns,
-                                emptyLooper, faberAddedByPhil, faberAgentPort):
-    newAgentName1 = "Agent11"
-    newAgentName2 = "Agent12"
-    with pytest.raises(PortNotAvailable):
-        agent1 = getNewAgent(newAgentName1, tdirWithPoolTxns, faberAgentPort,
-                    buildFaberWallet())
-        runAgent(emptyLooper, tdirWithPoolTxns, faberAgentPort,
-                 name=newAgentName2)
-        runAgent(emptyLooper, tdirWithPoolTxns, faberAgentPort,
-                 name=newAgentName1, agent=agent1)
-    emptyLooper.removeProdable(name=newAgentName1)
-    emptyLooper.removeProdable(name=newAgentName2)
-
-
-def testAgentStartedWithoutPoolStarted(emptyLooper, tdirWithPoolTxns,
-                                       faberAgentPort):
+def testAgentStartedWithoutPoolStarted(emptyLooper, tdirWithPoolTxns):
     newAgentName = "Agent2"
     with pytest.raises(NoConsensusYet):
-        runAgent(emptyLooper, tdirWithPoolTxns, faberAgentPort,
+        runAgent(emptyLooper, tdirWithPoolTxns, agentPort,
                  name=newAgentName)
     emptyLooper.removeProdable(name=newAgentName)
 
 
 def testStartAgentWithoutAddedToSovrin(poolNodesStarted, emptyLooper,
-                                 tdirWithPoolTxns, faberAgentPort):
+                                 tdirWithPoolTxns):
     newAgentName = "Agent3"
     with pytest.raises(OperationError) as oeinfo:
-        runAgent(emptyLooper, tdirWithPoolTxns, faberAgentPort,
+        runAgent(emptyLooper, tdirWithPoolTxns, agentPort,
                  name=newAgentName)
-    faberWallet = buildFaberWallet()
     assert "error occurred during operation: client request invalid: " \
-           "UnknownIdentifier('{}',)".format(faberWallet.defaultId) \
+           "UnknownIdentifier('{}',)".format(agentWallet().defaultId) \
            in str(oeinfo)
     emptyLooper.removeProdable(name=newAgentName)
 
 
 def testStartNewAgentOnUsedPort(poolNodesStarted, tdirWithPoolTxns,
-                                emptyLooper, faberAddedByPhil, faberAgentPort,
+                                emptyLooper, agentAddedBySponsor,
                                 agentStarted):
 
     with pytest.raises(PortNotAvailable):
-        runAgent(emptyLooper, tdirWithPoolTxns, faberAgentPort, name="Agent4")
+        runAgent(emptyLooper, tdirWithPoolTxns, agentPort, name="Agent4")
+
+
+def testStartAgentChecksForPortAvailability(poolNodesStarted, tdirWithPoolTxns,
+                                            emptyLooper, agentAddedBySponsor):
+    newAgentName1 = "Agent11"
+    newAgentName2 = "Agent12"
+    with pytest.raises(PortNotAvailable):
+        agent = getNewAgent(newAgentName1, tdirWithPoolTxns, agentPort,
+                            agentWallet())
+        runAgent(emptyLooper, tdirWithPoolTxns, agentPort,
+                 name=newAgentName2)
+        runAgent(emptyLooper, tdirWithPoolTxns, agentPort,
+                 name=newAgentName1, agent=agent)
+    emptyLooper.removeProdable(name=newAgentName2)

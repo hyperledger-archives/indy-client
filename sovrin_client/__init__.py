d58b49a5d84abc7fee63e5be55183584db5adb46
Andrei Goncharov <frantic1993@gmail.com>
2017-03-28 01:45:50 +0300

    Feature Added: Manual version check deprecation (#93)

--- sovrin_client/__init__.py
+++ sovrin_client/__init__.py
@@ -1,8 +1,5 @@
 import os
 import sovrin_client
-from plenum.common.pkg_util import check_deps
-
-check_deps(sovrin_client)
 
 from sovrin_common.plugin_helper import writeAnonCredPlugin
 BASE_DIR = os.path.join(os.path.expanduser("~"), ".sovrin")

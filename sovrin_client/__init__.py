6012fad5359189cf59885d0ddf7b124750843a73
Andrei Goncharov <frantic1993@gmail.com>
2017-03-28 13:43:54 +0300

    Hotfix: Deps (#94)

--- sovrin_client/__init__.py
+++ sovrin_client/__init__.py
@@ -1,6 +1,9 @@
 import os
 import sovrin_client
 
+import importlib
+from .__metadata__ import *
+
 from sovrin_common.plugin_helper import writeAnonCredPlugin
 BASE_DIR = os.path.join(os.path.expanduser("~"), ".sovrin")
 writeAnonCredPlugin(BASE_DIR)

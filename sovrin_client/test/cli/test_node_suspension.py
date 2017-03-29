c0b24b5733bdf6b5cd8a16923fd713fb20ac26cb
Lovesh <lovesh.harchandani@evernym.com>
2017-03-29 10:11:15 +0530

    updating test

--- sovrin_client/test/cli/test_node_suspension.py
+++ sovrin_client/test/cli/test_node_suspension.py
@@ -4,10 +4,23 @@ from sovrin_client.test.cli.test_node import newStewardCli, newStewardCLI, \
 
 
 def testSuspendNode(be, do, trusteeCli, newNodeAdded):
+    """
+    Suspend a node and then cancel suspension. Suspend while suspended
+    to test that there is no error
+    """
     nodeData = newNodeAdded
     be(trusteeCli)
     nodeData['newNodeData'][SERVICES] = []
     doNodeCmd(do, nodeData=nodeData)
+    # Re-suspend node
+    nodeData['newNodeData'][SERVICES] = []
+    doNodeCmd(do, nodeData=nodeData)
+
+    # Cancel suspension
+    nodeData['newNodeData'][SERVICES] = [VALIDATOR]
+    doNodeCmd(do, nodeData=nodeData)
+
+    # Re-cancel suspension
     nodeData['newNodeData'][SERVICES] = [VALIDATOR]
     doNodeCmd(do, nodeData=nodeData)
 

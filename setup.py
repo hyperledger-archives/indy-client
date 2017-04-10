b0beacbd508a1182e85ce02828687702e621f990
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-04-10 16:03:03 +0300

    increment version of sovrin-common

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.sovrin']},
     include_package_data=True,
-    install_requires=['sovrin-common-dev==0.2.25', 'anoncreds-dev==0.3.2'],
+    install_requires=['sovrin-common-dev==0.2.26', 'anoncreds-dev==0.3.2'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'sovrin-node-dev==0.3.15'],
     scripts=['scripts/sovrin', 'scripts/change_node_ha',

04a4b78be8ab4f016ba3e4ce72dccc98e47a4df6
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2017-04-27 21:30:21 +0100

    - Removed initial transactions and pool transactions files from sovrin-client project. - Removed creation of "sovrin_config.py" in "~/.sovrin" from sovrin-client project. - Commented out Setup.setupAll call in newGuyCLI fixture (not currently being used) since this method had been removed. Added a corresponding FIXME note.

--- setup.py
+++ setup.py
@@ -40,29 +40,18 @@ for path in [BASE_DIR, SAMPLE_DIR]:
     if not os.path.exists(path):
         os.makedirs(path)
 
-# create sovrin_config.py if not already exists
-CONFIG_FILE = os.path.join(BASE_DIR, "sovrin_config.py")
-if not os.path.exists(CONFIG_FILE):
-    with open(CONFIG_FILE, 'w') as f:
-        msg = "# Here you can create config entries according to your " \
-              "needs.\n " \
-              "# For help, refer config.py in the sovrin package.\n " \
-              "# Any entry you add here would override that from config " \
-              "example\n"
-        f.write(msg)
-
 
 def post_install():
     subprocess.run(['python post-setup.py'], shell=True)
 
 
-class PostInstall(install):
+class EnhancedInstall(install):
     def run(self):
         install.run(self)
         post_install()
 
 
-class PostInstallDev(develop):
+class EnhancedInstallDev(develop):
     def run(self):
         develop.run(self)
         post_install()
@@ -89,7 +78,7 @@ setup(
     scripts=['scripts/sovrin', 'scripts/change_node_ha',
              'scripts/add_new_node', 'scripts/reset_client'],
     cmdclass={
-        'install': PostInstall,
-        'develop': PostInstallDev
+        'install': EnhancedInstall,
+        'develop': EnhancedInstallDev
     }
 )

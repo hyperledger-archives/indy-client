9bd89a5a4c8ca1771f5dcf2c478a26043b669cd3
spivachuk <nikita.spivachuk@dsr-company.com>
2017-06-27 19:54:01 +0300

    INDY-260: Added parallel read-write tests (#237)
    * Moved the parallel read-write system tests from sovrin-node project to sovrin-client project.
    
    * Corrected usage examples in docstrings of the parallel read-write test scripts correspondingly to added hashbangs.
    
    * Corrected properties of command line arguments of the parallel read-write test scripts.
    
    * Corrected a docstring.

---
+++ scripts/test_some_write_keys_others_read_them.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+"""
+Test performing the following scenarios on behalf of multiple users in parallel:
+- some users cyclically update their own verkeys,
+- other users cyclically read verkeys of the former users.
+
+To run the test execute this python script providing the following parameters:
+-w <NUMBER_OF_USERS_UPDATING_VERKEYS> or --writers <NUMBER_OF_USERS_UPDATING_VERKEYS>
+-r <NUMBER_OF_USERS_READING_VERKEYS> or --readers <NUMBER_OF_USERS_READING_VERKEYS>
+-i <NUMBER_OF_ITERATIONS> or --iterations <NUMBER_OF_ITERATIONS>
+-t <TIMEOUT_IN_SECONDS> or --timeout <TIMEOUT_IN_SECONDS> (optional parameter)
+
+Examples:
+
+test_some_write_keys_others_read_them -w 2 -r 8 -i 10 -t 30
+
+test_some_write_keys_others_read_them --writers 4 --readers 20 --iteration 50
+"""
+
+import argparse
+from concurrent import futures
+from concurrent.futures import ProcessPoolExecutor
+
+from stp_core.common.log import getlogger
+
+from sovrin_client.utils.user_scenarios import generateNymsData, \
+    NymsCreationScenario, KeyRotationScenario, ForeignKeysReadScenario
+
+STEWARD1_SEED = b"000000000000000000000000Steward1"
+
+logger = getlogger()
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-w", "--writers",
+                        type=int,
+                        required=True,
+                        dest="writers",
+                        help="number of writers")
+
+    parser.add_argument("-r", "--readers",
+                        type=int,
+                        required=True,
+                        dest="readers",
+                        help="number of readers")
+
+    parser.add_argument("-i", "--iterations",
+                        type=int,
+                        required=True,
+                        dest="iterations",
+                        help="number of iterations")
+
+    parser.add_argument("-t", "--timeout",
+                        type=int,
+                        dest="timeout",
+                        help="timeout in seconds")
+
+    return parser.parse_args()
+
+
+def main(args):
+    numOfWriters = args.writers
+    numOfReaders = args.readers
+    numOfIterations = args.iterations
+    timeout = args.timeout
+
+    writers = generateNymsData(numOfWriters)
+    readers = generateNymsData(numOfReaders)
+
+    with ProcessPoolExecutor(numOfWriters + numOfReaders) as executor:
+        usersIdsAndVerkeys = [(user.identifier, user.verkey)
+                              for user in writers + readers]
+
+        nymsCreationScenarioFuture = \
+            executor.submit(NymsCreationScenario.runInstance,
+                            seed=STEWARD1_SEED,
+                            nymsIdsAndVerkeys=usersIdsAndVerkeys)
+
+        nymsCreationScenarioFuture.result(timeout=timeout)
+        logger.info("Created {} nyms".format(numOfWriters + numOfReaders))
+
+        keyRotationScenariosFutures = \
+            [executor.submit(KeyRotationScenario.runInstance,
+                             seed=writer.seed,
+                             iterations=numOfIterations)
+             for writer in writers]
+
+        writersIds = [writer.identifier for writer in writers]
+
+        foreignKeysReadScenariosFutures = \
+            [executor.submit(ForeignKeysReadScenario.runInstance,
+                             seed=reader.seed,
+                             nymsIds=writersIds,
+                             iterations=numOfIterations)
+             for reader in readers]
+
+        futures.wait(keyRotationScenariosFutures +
+                     foreignKeysReadScenariosFutures,
+                     timeout=timeout)
+
+        failed = False
+        for future in keyRotationScenariosFutures + \
+                foreignKeysReadScenariosFutures:
+            ex = future.exception(timeout=0)
+            if ex:
+                failed = True
+                logger.exception(ex)
+
+        if failed:
+            logger.error("Scenarios of some writers or readers failed")
+        else:
+            logger.info("Scenarios of all writers and readers "
+                        "finished successfully")
+
+
+if __name__ == "__main__":
+    main(parseArgs())
